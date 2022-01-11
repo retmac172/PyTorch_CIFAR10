cb665d04579caa313672f5a66a64e4ba078e3719
Taylan Göltaş <taylan.goltas@gmail.com>
2022-01-11 19:43:28 +0300

    Update train.py

--- train.py
+++ train.py
@@ -73,7 +73,7 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size", type=int, default=256)
     parser.add_argument("--max_epochs", type=int, default=100)
     parser.add_argument("--num_workers", type=int, default=8)
-    parser.add_argument("--gpu_id", type=str, default="1")
+    parser.add_argument("--gpu_id", type=str, default="0")
 
     parser.add_argument("--learning_rate", type=float, default=1e-2)
     parser.add_argument("--weight_decay", type=float, default=1e-2)
